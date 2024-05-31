2738fc06676a9e3405bddcd68e7877293351fe78
binbin-coder <149813206+binbin-coder@users.noreply.github.com>
2024-05-31 15:14:07 +0800

    Update model_or.py

--- STMGraph/model_or.py
+++ STMGraph/model_or.py
@@ -101,8 +101,8 @@ class SDGATE():
 
     def graph_attention_layer(self, A, M, v, layer):
         with tf.variable_scope("layer_%s" % layer):
-            f1 = tf.matmul(M, v)
-
+            # f1 = tf.matmul(M, v)
+            f1 = tf.transpose(tf.matmul(tf.nn.sigmoid(M),v),[1, 0])
             f1 = A * f1
             unnormalized_attentions1 = tf.SparseTensor(indices=f1.indices,
                                                        values=tf.nn.sigmoid(f1.values),
