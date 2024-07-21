826bab6d8f8bbabd26233a0b99b47ab61e941a28
binbin-coder <149813206+binbin-coder@users.noreply.github.com>
2024-07-21 22:59:38 +0800

    Update test_mouse_hippocampus_Slide.py

--- test_mouse_hippocampus_Slide.py
+++ test_mouse_hippocampus_Slide.py
@@ -8,7 +8,7 @@ from sklearn.metrics.cluster import adjusted_rand_score
 import STMGraph as STMGraph
 import argparse
 parser = argparse.ArgumentParser(description='manual to this script')
-parser.add_argument("--random-seed", type=int, default=100)
+parser.add_argument("--random-seed", type=int, default=52)
 parser.add_argument("--input-expression",type=str, default="/share/home/stu_qilin/software/stdata/6.Mouse_Hippocampus_Tissue/Puck_200115_08.digital_expression.txt")
 parser.add_argument("--input-locations",type=str, default="/share/home/stu_qilin/software/stdata/6.Mouse_Hippocampus_Tissue/Puck_200115_08_bead_locations.csv")
 parser.add_argument("--input-used-barcode",type=str, default="/share/home/stu_qilin/software/stdata/6.Mouse_Hippocampus_Tissue/used_barcodes.csv")
