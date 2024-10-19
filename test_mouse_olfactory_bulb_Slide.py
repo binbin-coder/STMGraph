7ed8b492972451b04f589ae83c7c5e3b6b962d08
binbin-coder <149813206+binbin-coder@users.noreply.github.com>
2024-10-20 00:41:01 +0800

    Update test_mouse_olfactory_bulb_Slide.py

--- test_mouse_olfactory_bulb_Slide.py
+++ test_mouse_olfactory_bulb_Slide.py
@@ -9,6 +9,7 @@ import STMGraph
 import argparse
 parser = argparse.ArgumentParser(description='manual to this script')
 parser.add_argument("--random-seed", type=int, default=52, help="Random seed for reproducibility")
+parser.add_argument("--num-cluster", type=int, default=11, help="Number of clusters to form")
 parser.add_argument("--input-expression",type=str, default="/share/home/stu_qilin/software/stdata/5.Mouse_Olfactory/Slide-seqV2_mouse_olfactory_bulb/Puck_200127_15.digital_expression.txt", help="Path to the count file")
 parser.add_argument("--input-locations",type=str, default="/share/home/stu_qilin/software/stdata/5.Mouse_Olfactory/Slide-seqV2_mouse_olfactory_bulb/Puck_200127_15_bead_locations.csv", help="Space coordinate file")
 parser.add_argument("--input-used-barcode",type=str, default="/share/home/stu_qilin/software/stdata/5.Mouse_Olfactory/Slide-seqV2_mouse_olfactory_bulb/used_barcodes.txt", help="Available barcode")
@@ -19,6 +20,7 @@ counts_file = args.input_expression
 coor_file = args.input_locations
 input_used_barcode=args.input_used_barcode
 output_dir = args.output_dir
+num_cluster=args.num_cluster
 counts = pd.read_table(counts_file, sep='\t', index_col=0)
 coor_df = pd.read_csv(coor_file, sep=',',index_col=0)
 coor_df = coor_df.set_index(coor_df.columns[2])
@@ -49,7 +51,7 @@ plt.close()
 
 adata.var["mt"] = adata.var_names.str.startswith("mt-")
 adata.var["ercc"] = adata.var_names.str.startswith("ercc-")
-adata = adata[:, ~(adata.var["mt"] | adata.var["ercc"])]#删除线粒体和外源基因
+adata = adata[:, ~(adata.var["mt"] | adata.var["ercc"])]
 
 sc.pp.filter_genes(adata, min_cells=3)
 sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=6000)
@@ -61,7 +63,7 @@ STMGraph.Cal_Spatial_Net(adata, rad_cutoff=50)
 # STAGATEV2.Stats_Spatial_Net(adata)
 
 adata = STMGraph.train_STMGraph(adata, mask_ratio=0.5,noise=0.05,alpha=1,random_seed=int(r),n_epochs=1500)
-adata = STMGraph.mclust_R(adata, used_obsm='STMGraph', num_cluster=11,random_seed=int(r))
+adata = STMGraph.mclust_R(adata, used_obsm='STMGraph', num_cluster=num_cluster,random_seed=int(r))
 # adata.obsm["spatial"] = adata.obsm["spatial"] * (-1)
 
 # adata.obsm['spatial'][:, 1] = -1*adata.obsm['spatial'][:, 1]
